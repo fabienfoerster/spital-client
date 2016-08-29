c8b653523bb8eff0e3224b67fcd56a7760e38bcf
Fabien Foerster <fabienfoerster@gmail.com>
2016-08-29 16:05:16 +0200

    remove static data use for poc poc poc

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
-    var data = [{"id":1,"name":"Chabichou","last_modified":1471705676091925633,"information":"","specialty":1},{"id":2,"name":"Valou","last_modified":1471705751371938459,"information":"","specialty":2},{"id":3,"name":"Fabien","last_modified":1471705764534589054,"information":"","specialty":3}];
-
     return (
       <div className="App">
         <div className="App-header">
@@ -17,7 +15,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <MuiThemeProvider>
-          <ListingView data={data} />
+          <ListingView />
         </MuiThemeProvider>
       </div>
     );
